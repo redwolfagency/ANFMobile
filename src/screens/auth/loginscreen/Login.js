42c7e81210cd1675b28ca3805634063679b577c2
Redwolf <benedicte.pancheri@gmail.com>
2020-10-01 15:20:41 +0200

    Add Button, text backgroundImage Css

--- src/screens/auth/loginscreen/Login.js
+++ src/screens/auth/loginscreen/Login.js
@@ -1,24 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {screens} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+
 
 
 export default function Login(){
    
     return (
-        <View style={styles.container}>
-            <Text>bruhhhh</Text>
-        </View>
+        <ImageBackground source={require('./img/LoginPircture.jpg')} style={styles.loginImage}>
+            <View style={styles.overlay}>
+            <View style={styles.contentText}>
+                <View style={styles.viewButton}>
+                    <Button title="S'inscrire"/>
+                    <Button  title="Connexion"/>
+                </View>
+                <Text style={styles.text}>Mot de passe oublié ?</Text>
+             </View>
+            </View>
+            
+            
+        </ImageBackground>
     );
 }
 
 
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    loginImage:{
+        flex:1,
+        backgroundColor: 'rgba(255,0,0,.6)',
+        width: "100%",
+        height: "100%",
+        
+    },
+    overlay:{
+        backgroundColor: 'rgba(7,7,113,0.6)',
+        display: "flex",
+        justifyContent: 'flex-end',
+        height:"100%",
+    },
+    contentText:{
+        alignItems:"center",
+        justifyContent:"space-between",
+        height:"40%",
+        
+    },
+    viewButton:{
+        padding:10,
+        width: "50%",
+        height:"10%",
+    },
+    text:{
+        color:"white",
+        fontSize:16,
+    }
+   
 });
